daa1cb7ebc52a1412907533e23c627ca78060629
pHYo <86188804+pHYo1328@users.noreply.github.com>
2023-10-23 20:29:54 +0800

    created simple crud procedures

--- speedrun-v1/src/server/api/routers/notesRouter.ts
+++ speedrun-v1/src/server/api/routers/notesRouter.ts
@@ -1,22 +1,79 @@
-import { createTRPCRouter,publicProcedure } from '../trpc';
-import {z} from 'zod';
+import { createTRPCRouter, publicProcedure } from '../trpc';
+import { z } from 'zod';
 import { db } from '@/server/db';
+const handleError = (error: Error, message: string) => {
+    // Log the error for debugging
+    console.error(error);
+
+    // Here you can add more complex logic, like sending the error to an error-tracking service
+
+    throw new Error(`${message}: ${error.message}`);
+};
+
 export const noteRouter = createTRPCRouter({
-    noteslist: publicProcedure.query(async()=>{
-        const notes = await db.notes.findMany();
-        return notes;
+    noteslist: publicProcedure.query(async () => {
+        try {
+            const notes = await db.notes.findMany();
+            return notes;
+        } catch (err) {
+            handleError(err as Error, 'Failed to fetch all notes');
+        }
     }
     ),
     notesCreate: publicProcedure.input(z.object({
         title: z.string(),
-        description : z.string(),
-    })).query(async(opts)=>{
-        const {input} = opts;
-        const newNote = await db.notes.create({
-            data :{
-                title : input.title,
-                desc     : input.description
-            }
-        }) 
+        description: z.string(),
+    })).query(async (opts) => {
+        try {
+            const { input } = opts;
+            const newNote = await db.notes.create({
+                data: {
+                    title: input.title,
+                    desc: input.description
+                }
+            })
+            return newNote;
+        } catch (err) {
+            handleError(err as Error, 'Failed to create note');
+        }
     }),
+    notesUpdate: publicProcedure
+        .input(z.object({
+            id: z.number(),
+            title: z.string().optional(),
+            description: z.string().optional(),
+        }))
+        .mutation(async (opts) => {
+            try {
+                const { input } = opts;
+
+                const updatedNote = await db.notes.update({
+                    where: { id: input.id },
+                    data: {
+                        title: input.title,
+                        desc: input.description,
+                    },
+                });
+                return updatedNote;
+            } catch (err) {
+                handleError(err as Error, 'Failed to update note');
+            }
+
+        }),
+    notesDelete: publicProcedure
+        .input(z.object({
+            id: z.number(),
+        }))
+        .mutation(async (opts) => {
+            try {
+                const { input } = opts;
+
+                const deletedNote = await db.notes.delete({
+                    where: { id: input.id },
+                });
+                return deletedNote;
+            } catch (err) {
+                handleError(err as Error, 'Failed to delete note');
+            }
+        }),
 });
